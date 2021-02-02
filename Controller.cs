c39243c9319e322ba3fd3b5d7ba2f58b3428242e
yannihilator <64179787+yannihilator@users.noreply.github.com>
2021-02-01 21:58:16 -0500

    initial version has decent functionality. other features need to be finalized

--- Controller.cs
+++ Controller.cs
@@ -2,19 +2,55 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using TimeLog.Models;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace TimeLog
 {
     public static class Controller
     {
         private static List<LogEntry> entries;
+
+        private static string SerializedEntries()
+        {
+            return JsonSerializer.Serialize(entries);
+        }
+
+        public static void PopulateEntries()
+        {
+            List<LogEntry> _entries = new List<LogEntry>();
+            using (StreamReader r = new StreamReader("/Users/yanni/Documents/Repos/TimeLog/data.json"))
+            {
+                string json = r.ReadToEnd();
+                if (json != null && json != string.Empty) entries = JsonSerializer.Deserialize<List<LogEntry>>(json);         
+            }
+        }
+
         public static List<LogEntry> GetAllLogEntries()
         {
             return entries;
         }
+        
         public static List<LogEntry> GetTodaysLogEntries()
         {
             return entries?.Where(x => x.StartTime.Date == DateTime.Now.Date).ToList();
+        }
+
+        public static int GetNextId()
+        {
+            return entries.Count > 0 ? entries.Select(x => x.Id).Max() + 1 : 1;
+        }
+        public static List<string> ChargeNumbers()
+        {
+            return entries.Select(x => x.ChargeNumber).Distinct().ToList();
+        }
+
+        public static void AddEntry(LogEntry entry)
+        {
+            entry.Id = GetNextId();
+            entries.Add(entry);
+            File.WriteAllText("/Users/yanni/Documents/Repos/TimeLog/data.json", SerializedEntries());
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Linq;
 using TimeLog.Models;
+using System.Timers;
 
 namespace TimeLog
 {
     class Program
     {
+        private static Timer timer = new Timer() {Interval = 1000};
+        private static LogEntry currentEntry;
+
         static void Main(string[] args)
         {
+            //subscribes timer to tick event
+            timer.Elapsed += delegate{TimerTick();};
+
+            //Populates entries in memory and calls user interface to be populated
+            Controller.PopulateEntries();
+            UserInterface();
+        }
+
+        static void UserInterface()
+        {   
+            Console.Clear();
+            //Populates Today's entries part of the UI       
             var entries = Controller.GetTodaysLogEntries();
-            Console.WriteLine($"Entries for today, {DateTime.Now.ToShortDateString()}");
+            Console.WriteLine($"\nEntries for today, {DateTime.Now.ToShortDateString()}");
             Console.WriteLine("*******************************************\n");
             if (entries?.Count > 0)
             {
@@ -21,22 +37,32 @@ namespace TimeLog
                 }
             }
             else Console.WriteLine("No entries for today\n");
-            Console.WriteLine("*******************************************");
+            Console.WriteLine("\n*******************************************");
 
-            UserInterface();
-        }
-
-        static void UserInterface()
-        {          
-            Console.WriteLine("\nType in an entry's ID and press Enter to take an action, or press the enter key to start the timer.");
+            //action part of the UI begins here
+            Console.WriteLine("\nAvailable Actions: \n\n");
+            Console.WriteLine("-###       |   Entry ID to take an action for it.\n");
+            Console.WriteLine("-[Enter]   |   Start the timer.\n");
+            Console.WriteLine("-total     |   Show daily totals.\n");
+            Console.WriteLine("-stop      |   Exit Application.\n");
             var output = Console.ReadLine();
-            if(output == string.Empty)
-            {
-                Console.WriteLine("timer starting");
-            }
-            else if (output.All(x => char.IsNumber(x)))
+
+            if (output.All(x => char.IsNumber(x)))
             {
                 Console.WriteLine("what action would you like to take");
+            }
+            else if(output == string.Empty)
+            {
+                Console.WriteLine("Press the enter key to stop the timer.\n");
+                StartTimer();
+            }
+            else if (output.ToLower() == "total")
+            {
+
+            }
+            else if (output.ToLower() == "stop")
+            {
+                
             }
             else
             {
@@ -45,14 +71,55 @@ namespace TimeLog
             }
         }
 
+        static void TimerTick()
+        {
+            Console.Write($"\r{(DateTime.Now - currentEntry.StartTime).ToString("hh\\:mm\\:ss")}");
+        }
+
         static void StartTimer()
         {
+            if (currentEntry == null) currentEntry = new LogEntry();
+            currentEntry.StartTime = DateTime.Now;
+            Console.ForegroundColor = ConsoleColor.Green;
+            timer.Start();
 
+            var output = Console.ReadLine();
+            if (output == string.Empty) StopTimer();
         }
 
         static void StopTimer()
         {
+            timer.Stop();
+            currentEntry.EndTime = DateTime.Now;
+            Console.ResetColor();
+            Console.WriteLine($"\nWhat is the charge number?\n");
+            
+            var chargeNumbers = Controller.ChargeNumbers();
+            int counter = 0;
+            foreach (string chargeNumber in chargeNumbers) 
+            {
+                Console.WriteLine($"{counter} - {chargeNumber}");
+                counter++;
+            }
 
+            var output = Console.ReadLine();
+            if (output.All(x => char.IsNumber(x)))
+            {
+                currentEntry.ChargeNumber = chargeNumbers[Convert.ToInt32(output)];
+            }
+            else if (output != string.Empty)
+            {
+                currentEntry.ChargeNumber = output;
+            }
+            else
+            {
+                currentEntry.ChargeNumber = "null";
+            }
+
+            Controller.AddEntry(currentEntry);
+            currentEntry = null;
+            Console.Clear();
+            UserInterface();
         }
     }
 }
